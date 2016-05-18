55e3c3a63c68cebb12b3b3a25f10f5090785c2e5
vinikkk <vinipereira3@live.com>
2016-05-18 17:12:38 -0300

    Initial Object Header

--- TrabCG/TrabCG/Object.h
+++ TrabCG/TrabCG/Object.h
@@ -1,8 +1,27 @@
 #pragma once
+
+#include <string>
+#include "Mesh.h"
+#include "Shader.h"
+
 class Object
 {
 public:
+	//DATA
+	string objectId;
+	Shader shader;
+	
+	//FUNCTIONS
 	Object();
-	~Object();
+	~Object(GLchar* path) { this->loadModel(path); };
+	void Draw();
+	
+private:
+	vector<Mesh> meshes;
+	string directory;
+        void loadModel(string path);
+        void processNode(aiNode* node, const aiScene* scene);
+        Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+        vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type, string typeName);
 };
 
